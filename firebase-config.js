5d68269b35f441447a819f2f658bb3b263bd2b4f
Mateus de Oliveira <47123576+Mateuzzim@users.noreply.github.com>
2026-09-11 20:03:41 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const FirebaseConfig = {
-  apiKey: "AIzaSyBLmJ9I5eoeNQon82h0qUWxpq2nbAl1KfI",
-  authDomain: "robobjw-dce90.firebaseapp.com",
-  projectId: "robobjw-dce90",
-  storageBucket: "robobjw-dce90.firebasestorage.app",
-  messagingSenderId: "860690435513",
-  appId: "1:860690435513:web:1faf632e24f5d8ea4fba68"
-};
